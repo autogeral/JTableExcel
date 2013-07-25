a16f78401777ed82256da22b5d819c815875f020
murilo <murilo@5adba155-f38a-43c0-aea0-d88c706dde45>
2013-07-25 17:41:47 +0000

    [update] exportando tanto para XLS quanto XLSX sendo que XLSX eh o padrao
    git-svn-id: https://svn.autogeral.com.br/code/JTableExcel@3388 5adba155-f38a-43c0-aea0-d88c706dde45

--- src/br/com/jcomputacao/jtableexcel/ExcelExporter.java
+++ src/br/com/jcomputacao/jtableexcel/ExcelExporter.java
@@ -8,6 +8,11 @@ import org.apache.poi.hssf.usermodel.HSSFRichTextString;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRichTextString;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  * 16/09/2011 11:42:53
@@ -18,10 +23,19 @@ public class ExcelExporter {
     private final TableModel tableModel;
     private final OutputStream destination;
     private String sheetName;
+    private boolean xlsx = true;
 
     public ExcelExporter(TableModel model, OutputStream destination) {
         this.tableModel = model;
         this.destination = destination;
+    }
+    
+    public void setExportToXls() {
+        this.xlsx = false;
+    }
+    
+    public void setExportToXlsx() {
+        this.xlsx = true;
     }
 
     public void setSheetName(String sheetName) {
@@ -29,6 +43,14 @@ public class ExcelExporter {
     }
 
     public void execute() throws IOException {
+        if(xlsx) {
+            executeXlsx();
+        } else {
+            executeXls();
+        }
+    }
+    
+    private void executeXls() throws IOException {
         HSSFWorkbook workbook = new HSSFWorkbook();
         if (this.sheetName == null || this.sheetName.trim().equals("")) {
             this.sheetName = "JTable";
@@ -39,7 +61,19 @@ public class ExcelExporter {
         createBody(sheet);
 
         workbook.write(destination);
+    }
+    
+    private void executeXlsx() throws IOException {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        if (this.sheetName == null || this.sheetName.trim().equals("")) {
+            this.sheetName = "JTable";
+        }
+        XSSFSheet sheet = workbook.createSheet(this.sheetName);
 
+        createHeader(sheet);
+        createBody(sheet);
+
+        workbook.write(destination);
     }
 
     private void createHeader(HSSFSheet sheet) {
@@ -53,7 +87,19 @@ public class ExcelExporter {
             cell.setCellValue(new HSSFRichTextString(columnName));
         }
     }
+    
+    private void createHeader(XSSFSheet sheet) {
+        int cols = tableModel.getColumnCount();
+        XSSFRow row = sheet.createRow(0);
 
+        for (int i = 0; i < cols; i++) {
+            String columnName = tableModel.getColumnName(i);
+            XSSFCell cell = row.createCell(i);
+            cell.setCellType(XSSFCell.CELL_TYPE_STRING);
+            cell.setCellValue(new XSSFRichTextString(columnName));
+        }
+    }
+    
     private void createBody(HSSFSheet sheet) {
         int cols = tableModel.getColumnCount();
         int rows = tableModel.getRowCount();
@@ -71,6 +117,23 @@ public class ExcelExporter {
         }
     }
 
+    private void createBody(XSSFSheet sheet) {
+        int cols = tableModel.getColumnCount();
+        int rows = tableModel.getRowCount();
+
+        for (int i = 0; i < rows; i++) {
+            XSSFRow row = sheet.createRow(i+1);
+            for (int j = 0; j < cols; j++) {
+                XSSFCell cell = row.createCell(j);
+
+                Object value = tableModel.getValueAt(i, j);
+                if (value != null) {
+                    defineCell(cell, value);
+                }
+            }
+        }
+    }
+    
     private void defineCell(HSSFCell cell, Object value) {
         if (value instanceof Double || value instanceof Float
                 || value instanceof Long || value instanceof Integer) {
@@ -87,4 +150,22 @@ public class ExcelExporter {
             cell.setCellValue(new HSSFRichTextString(value.toString()));
         }
     }
+
+    private void defineCell(XSSFCell cell, Object value) {
+        if (value instanceof Double || value instanceof Float
+                || value instanceof Long || value instanceof Integer) {
+            cell.setCellType(XSSFCell.CELL_TYPE_NUMERIC);
+            cell.setCellValue(new Double(value.toString()));
+        } else if (value instanceof Boolean) {
+            cell.setCellType(XSSFCell.CELL_TYPE_BOOLEAN);
+            cell.setCellValue(Boolean.valueOf(value.toString()));
+//        } else if (value instanceof java.util.Date || value instanceof java.util.Calendar) {
+//            cell.setCellType(HSSFCell.CELL_TYPE_STRING);
+//            cell.setCellValue(new Double(value.toString()));
+        } else {
+            cell.setCellType(XSSFCell.CELL_TYPE_STRING);
+            cell.setCellValue(new XSSFRichTextString(value.toString()));
+        }
+    }
+
 }
