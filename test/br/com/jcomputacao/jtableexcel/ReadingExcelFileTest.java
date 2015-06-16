b6c293c2c0a400f7c79df9bf1cd631b7db80a099
murilo <murilo@5adba155-f38a-43c0-aea0-d88c706dde45>
2015-06-16 03:02:41 +0000

    [add] lendo arquivo do excel
    git-svn-id: https://svn.autogeral.com.br/code/JTableExcel@6585 5adba155-f38a-43c0-aea0-d88c706dde45

---
+++ test/br/com/jcomputacao/jtableexcel/ReadingExcelFileTest.java
@@ -0,0 +1,79 @@
+package br.com.jcomputacao.jtableexcel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 15/06/2015 23:43:32
+ * @author murilo
+ */
+public class ReadingExcelFileTest {
+
+    public ReadingExcelFileTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void hello() throws FileNotFoundException, IOException {
+        File excel = new File("cellStyleExample.xlsx");
+        FileInputStream fis = new FileInputStream(excel);
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+        int sheets = wb.getNumberOfSheets();
+        for (int sheet = 0; sheet < sheets; sheet++) {
+            String sheetName = wb.getSheetName(sheet);
+            XSSFSheet ws = wb.getSheet(sheetName);
+
+            int rowNum = ws.getLastRowNum() + 1;
+            int colNum = ws.getRow(0).getLastCellNum();
+            String[][] data = new String[rowNum][colNum];
+
+            for (int i = 0; i < rowNum; i++) {
+                XSSFRow row = ws.getRow(i);
+                for (int j = 0; j < colNum; j++) {
+                    XSSFCell cell = row.getCell(j);
+                    XSSFCellStyle style = cell.getCellStyle();
+                    String value = cell.toString();
+                    data[i][j] = value;
+                    System.out.println("The value in sheet " + sheetName + ", line " + i + " and column " + j + " is " + value);
+                    System.out.println("Fill Pattern       : " + style.getFillPatternEnum());
+                    System.out.println("Background Color   : " + style.getFillBackgroundColor());
+                    System.out.println("Foreground Color   : " + style.getFillForegroundColor());
+                    System.out.println("Font theme Color   : " + style.getFont().getThemeColor());
+                    System.out.println("Font family        : " + style.getFont().getFamily());
+                    System.out.println("Font Color         : " + style.getFont().getColor());
+                }
+            }
+        }
+
+        System.out.println("Ok");
+    }
+
+}
